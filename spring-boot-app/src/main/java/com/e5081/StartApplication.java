0b3eb67db9eed86361cdb5d09f1bd7f1c571798c
Ajeet Singh <singhajeet77@gmail.com>
2024-05-24 13:09:34 +0530

    replacing root to e5081 in Jenkinsfile

--- spring-boot-app/src/main/java/com/e5081/StartApplication.java
+++ spring-boot-app/src/main/java/com/e5081/StartApplication.java
@@ -1,4 +1,4 @@
-package com.abhishek;
+package com.e5081;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +13,7 @@ public class StartApplication {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
+        model.addAttribute("msg", "This application is deployed onto a Kubernetes cluster using Argo CD");
         return "index";
     }
 
